e20cc28bad8f703f4c3dd881513b6a19d4e6d60b
Nicolas Chan <nicolas@nicolaschan.com>
2019-07-05 13:49:13 -0700

    Rename prices to bank-config

--- slurm_banking/src/prices_config.rs
+++ slurm_banking/src/prices_config.rs
@@ -4,8 +4,8 @@ use config::{Config, File};
 use super::logging::safe_info;
 
 /// The price config file path, without the file extension
-/// For example, for /etc/slurm/prices.toml write /etc/slurm/prices
-static PRICES_CONFIG_FILE_PATH: &str = "/etc/slurm/prices";
+/// For example, for /etc/slurm/bank-config.toml write /etc/slurm/bank-config
+static PRICES_CONFIG_FILE_PATH: &str = "/etc/slurm/bank-config";
 
 fn log(message: &str) {
     safe_info(&("slurm_banking_lib: ".to_owned() + message))
