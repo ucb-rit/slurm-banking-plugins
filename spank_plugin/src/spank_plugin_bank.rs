1b34b4df31a133b17bc076dbfa42b0ec4780860a
Nicolas Chan <nicolas@nicolaschan.com>
2019-08-28 11:49:40 -0700

    Spank reports num_alloc_nodes

--- spank_plugin/src/spank_plugin_bank.rs
+++ spank_plugin/src/spank_plugin_bank.rs
@@ -118,7 +118,8 @@ pub extern "C" fn slurm_spank_init(sp: spank_t, _ac: c_int, _argv: *const *const
         .with_startdate(start_timestamp_str)
         .with_submitdate(submit_timestamp_str)
         .with_nodes(nodes)
-        .with_num_cpus(num_cpus as i32);
+        .with_num_cpus(num_cpus as i32)
+        .with_num_alloc_nodes(num_nodes as i32);
 
     log(&format!("Creating job wih info: {:?}", job_create_record));
     let base_path = slurm_banking::prices_config::get_base_path(&conf);
