9e9490d2c9d837511226aafc29c00398bc6be89a
Nicolas Chan <nicolas@nicolaschan.com>
2019-12-02 10:34:43 -0800

    Add remove slurm.h

--- scripts/build-with-docker.sh
+++ scripts/build-with-docker.sh
@@ -3,6 +3,7 @@ SLURM_TAG=$1
 DOCKER_IMAGE=$2
 cd slurm && git checkout "$SLURM_TAG" && cd ..
 sudo make clean
+sudo rm -f slurm/slurm/slurm.h
 docker run -v $(pwd):/tmp "$DOCKER_IMAGE"
 
 PLUGIN_DIR="plugins-$SLURM_TAG"
