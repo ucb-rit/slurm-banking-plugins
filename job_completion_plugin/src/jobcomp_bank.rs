4c7ca3e10f36903a49817c4fb79a78c238b279a0
Nicolas Chan <nicolaschan@berkeley.edu>
2020-09-02 11:21:47 -0700

    Fix formatting

--- job_completion_plugin/src/jobcomp_bank.rs
+++ job_completion_plugin/src/jobcomp_bank.rs
@@ -92,11 +92,10 @@ pub extern "C" fn slurm_jobcomp_log_record(job_ptr: *const job_record) -> u32 {
         Some(account) => account,
         None => return ESLURM_INVALID_ACCOUNT,
     };
-    let partition: String =
-        match safe_helpers::deref_cstr(unsafe { (*(*job_ptr).part_ptr).name }) {
-            Some(partition) => partition,
-            None => return ESLURM_INVALID_PARTITION_NAME,
-        };
+    let partition: String = match safe_helpers::deref_cstr(unsafe { (*(*job_ptr).part_ptr).name }) {
+        Some(partition) => partition,
+        None => return ESLURM_INVALID_PARTITION_NAME,
+    };
     let qos: String = match safe_helpers::deref_cstr(unsafe { (*(*job_ptr).qos_ptr).name }) {
         Some(qos) => qos,
         None => return ESLURM_INVALID_QOS,
